86945d28e4d3a89cf40b6d85020764964ff53ec4
Angel-Miguel-Valencia-Carrillo <a.22308051280738@cbtis128.edu.mx>
2024-09-20 09:30:04 -0600

    Add files via upload

---
+++ operadoresA.py
@@ -0,0 +1,45 @@
+print("Act 9 clases v2")
+print("Angel Valencia Mat: 22308051280738")
+# zona de class
+class operadores0738:
+    # zona de funciones
+    def suma0738(self,E,N):
+        s0738=E+N
+        print(f"La suma de {E} + {N} = {s0738}")
+    def resta0738(self,A,V):
+        r0738=A-V
+        print(f"La resta de {A} - {V} = {r0738}")
+    def multi0738(self,F,L):
+        m0738=F*L
+        print(f"La multiplicacion de {F} * {L} = {m0738}")
+    def divi0738(self,M,D):
+        d0738=M/D
+        print(f"La division de {M} / {D} = {d0738}")
+    def modulo0738(self,S,Z):
+        mo0738=S%Z
+        print(f"El modulo de {S} % {Z} = {mo0738}")
+    def exponente0738(self,K,T):
+        e0738=K**T
+        print(f"El exponente de {K} ** {T} = {e0738}")
+    def cociente0738(self,P,C):
+        c0738=P//C
+        print(f"El cociente de {P} // {C} = {c0738}")
+
+# zona de creacion del objeto 
+operaAngel= operadores0738()
+
+# zona de uso 
+print(" \n Funcion suma ")
+operaAngel.suma0738(5,4)
+print("\n Funcion resta ")
+operaAngel.resta0738(6,7)
+print("\n Funcion multiplicacion ")
+operaAngel.multi0738(8,43)
+print("\n Funcion division ")
+operaAngel.divi0738(55,44)
+print("\n Funcion modulo ")
+operaAngel.modulo0738(75,40)
+print("\n Funcion exponente ")
+operaAngel.exponente0738(8,5)
+print("\n Funcion cociente ")
+operaAngel.cociente0738(9,3)
